c422a4c4dcbb8c3c9c7735971ee1d25a02dd855d
Alex Hopper <94771055+Alex-Hopper@users.noreply.github.com>
2026-08-02 19:09:23 -0700

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+
+        # build graph
+        for course, prereq in prerequisites:
+            graph[course].append(prereq)
+
+        def done(cr, visited) -> bool:
+            if cr in visited:
+                return False
+            visited.add(cr)
+            graph[cr] = [c for c in graph[cr] if not done(c, visited)]
+            visited.remove(cr)
+            return len(graph[cr]) == 0
+
+            # if cr2 in graph[cr]:
+            #     return False
+
+            # graph[cr] = [c for c in graph[cr] if not done(c, cr)]
+
+            # return len(graph[cr]) == 0
+
+        for i in range(numCourses):
+            if not done(i, set()):
+                return False
+                
+        return True
+
+        
