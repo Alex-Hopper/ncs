13b3e5ec42829d734d8521e71fc7a03ffdfa1fee
Alex Hopper <94771055+Alex-Hopper@users.noreply.github.com>
2026-08-11 19:18:11 -0700

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = {}
+
+        def dp(idx1, idx2):
+            if idx1 >= len(text1) or idx2 >= len(text2):
+                return 0
+            
+            if (idx1, idx2) in memo:
+                return memo[(idx1, idx2)]
+
+            if text1[idx1] == text2[idx2]:
+                return 1 + dp(idx1 + 1, idx2 + 1)
+            
+            memo[(idx1, idx2)] = max(dp(idx1 + 1, idx2), dp(idx1, idx2 + 1))
+            return memo[(idx1, idx2)]
+            
+        return dp(0, 0)
